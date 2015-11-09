830fa8bd0b82fed420eed838d1cc0de8827f19b1
Rahul Ballal <rahulballal@gmail.com>
2015-11-08 18:02:21 -0800

    fixes build.ps1 and appveyor yml

--- build_automation/build.ps1
+++ build_automation/build.ps1
@@ -6,42 +6,42 @@ task default -depends cleanup,init,local
 
 task local -depends compile,unittest,inttest,pack,zip
 
-task init -description 'Initialize output folders'
+task init -description "Initialize output folders"
 {
     Do-Init
 }
 
-task info -description 'Prints information about the current working folders'
+task info -description "Prints information about the current working folders"
 {
     Get-Info
 }
 
-task  restore -description 'Performs nuget restore'
+task  restore -description "Performs nuget restore"
 {
     Do-NugetRestore
 }
 
-task compile -depends init,restore -description 'Performs MSBuild operation'
+task compile -depends init,restore -description "Performs MSBuild operation"
 {
     Do-Compile
 }
 
-task unittest -description 'Run unit tests using configured test runner'
+task unittest -description "Run unit tests using configured test runner"
 {
     Do-UnitTest
 }
 
-task inttest -description 'Run integration tests using the configured test runner'
+task inttest -description "Run integration tests using the configured test runner"
 {
     Do-IntTest
 }
 
-task pack -description 'Performs nuget pack command'
+task pack -description "Performs nuget pack command"
 {
     Do-NugetPack
 }
 
-task zip -description 'Performs zip task using 7Zip (default)'
+task zip -description "Performs nuget pack command"
 {
     Do-7Zip
 }
